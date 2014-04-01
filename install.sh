2d9a0e3446978f7568d2b1408aa3e07e75305ef4
Shankar Manamalkav <mnshankar@yahoo.com>
2014-04-01 12:19:41 -0400

    Update install.sh
    Add composer vendor global directory to zshrc path

--- install.sh
+++ install.sh
@@ -71,8 +71,8 @@ sudo su - vagrant -c 'wget https://github.com/robbyrussell/oh-my-zsh/raw/master/
 
 # Set default to the lovely "ys" theme.
 sudo sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="ys"/' /home/vagrant/.zshrc
-# Add /sbin to PATH
-sudo sed -i 's=:/bin:=:/bin:/sbin:/usr/sbin:=' /home/vagrant/.zshrc
+# Add /sbin to PATH.. And phpunit/codeception
+sudo sed -i 's=:/bin:=:/bin:/sbin:/usr/sbin:~/.composer/vendor/bin:=' /home/vagrant/.zshrc
 # Change vagrant user's default shell
 chsh vagrant -s $(which zsh);
 
